1c0d23b7123887dc6bb33f1acaa750bafb9726a9
Lukas Körfer <l.koerfer@wzl.rwth-aachen.de>
2018-12-06 17:47:31 +0100

    Reduce Gradle version and add methods for writing

--- src/main/java/de/lukaskoerfer/simplepnml/PNML.java
+++ src/main/java/de/lukaskoerfer/simplepnml/PNML.java
@@ -6,30 +6,40 @@ import org.simpleframework.xml.Root;
 import org.simpleframework.xml.Serializer;
 import org.simpleframework.xml.core.Persister;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Reader;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-@Root
+@Root(name = "pnml")
 @Namespace(reference = "http://www.pnml.org/version-2009/grammar/pnml")
 public class PNML {
 
     @ElementList(required = false, inline = true)
     private List<Net> nets = new ArrayList<>();
-
-    public static Serializer buildSerializer() {
-        Persister persister = new Persister();
-        return persister;
+     
+    public void write(OutputStream target) throws Exception {
+    	buildSerializer().write(this, target);
     }
-
-    public static PNML read(File source) throws Exception {
-        return buildSerializer().read(PNML.class, source);
+    
+    public void write(Writer target) throws Exception {
+    	buildSerializer().write(this, target);
     }
-
-    public static PNML read(String source) throws Exception {
-        return buildSerializer().read(PNML.class, source);
+    
+    public void write(File target) throws Exception {
+    	buildSerializer().write(this, target);
+    }
+    
+    public String write() throws Exception {
+    	try (ByteArrayOutputStream stream = new ByteArrayOutputStream()) {
+    		write(stream);
+    		return new String(stream.toByteArray(), StandardCharsets.UTF_8);
+    	}
     }
 
     public static PNML read(InputStream source) throws Exception {
@@ -39,6 +49,17 @@ public class PNML {
     public static PNML read(Reader source) throws Exception {
         return buildSerializer().read(PNML.class, source);
     }
-
-
+    
+    public static PNML read(File source) throws Exception {
+        return buildSerializer().read(PNML.class, source);
+    }
+    
+    public static PNML read(String source) throws Exception {
+        return buildSerializer().read(PNML.class, source);
+    }
+    
+    protected static Serializer buildSerializer() {
+        Persister persister = new Persister();
+        return persister;
+    }
 }
